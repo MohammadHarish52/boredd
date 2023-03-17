54a32637766a4e173a454a85c06ec704f2907870
tachyonok <109061798+tachyonok@users.noreply.github.com>
2023-03-17 19:57:44 +0530

    bored fun
    i was bored

---
+++ site.js
@@ -0,0 +1,7 @@
+alert("check if you are madarchod");
+var c= prompt("enter you name ");
+if(c==="nainish"||c==="Nainish"){
+    alert("you are a consfirmed madarchod");}
+    else{
+        alert("you are a good boy");
+    }
